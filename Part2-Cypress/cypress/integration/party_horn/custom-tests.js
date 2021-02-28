7be21f3fa08ebf97300507b7f990e54d7f6f1495
Diego Alfaro <diegojalfaro@gmail.com>
2021-02-27 22:38:11 -0800

    finish tests

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -1,9 +1,124 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress');
   });
 
   it('First Test', () => {
     expect(true).to.equal(true);
+  });
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number')
+      .clear()
+      .type('75');
+    cy.get('#volume-slider')
+      .then(($el) => {
+        expect($el).to.have.value(75);
+      });
+  });
+
+  it('Volume input changes when slider changes', () => {
+    cy.get('#volume-slider')
+      .invoke('val', 33)
+      .trigger('input');
+    cy.get('#volume-number')
+      .then(($el) => {
+        expect($el).to.have.value(33);
+      });
+  });
+
+  it('Volume changes when slider changes', () => {
+    cy.get('#volume-slider')
+      .invoke('val', 33)
+      .trigger('input');
+    cy.get('#horn-sound')
+      .then(($el) => {
+        expect($el).to.have.prop('volume', 0.33);
+      });
+  });
+
+  it('Image and sound sources on party horn select', () => {
+    cy.get('#radio-party-horn')
+      .check()
+      .trigger('change');
+    cy.get('#sound-image')
+      .then(($el) => {
+        expect($el).to.have.attr('src', './assets/media/images/party-horn.svg');
+      });
+    cy.get('#horn-sound')
+      .then(($el) => {
+        expect($el).to.have.attr('src', './assets/media/audio/party-horn.mp3');
+      });
+  });
+
+  it('Volume image changes when increasing volume', () => {
+    cy.get('#volume-number')
+      .clear()
+      .type('0')
+      .trigger('input');
+    cy.get('#volume-image')
+      .then(($el) => {
+        expect($el).to.have.attr('src', './assets/media/icons/volume-level-0.svg');
+      });
+
+    cy.get('#volume-number')
+      .clear()
+      .type('33')
+      .trigger('input');
+    cy.get('#volume-image')
+      .then(($el) => {
+        expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
+      });
+
+    cy.get('#volume-number')
+      .clear()
+      .type('66')
+      .trigger('input');
+    cy.get('#volume-image')
+      .then(($el) => {
+        expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
+      });
+
+    cy.get('#volume-number')
+      .clear()
+      .type('100')
+      .trigger('input');
+    cy.get('#volume-image')
+      .then(($el) => {
+        expect($el).to.have.attr('src', './assets/media/icons/volume-level-3.svg');
+      });
+  });
+
+  it('Honk button disabled when input invalid', () => {
+    cy.get('#volume-number')
+      .clear()
+      .trigger('input');
+    cy.get('#honk-btn')
+      .then(($el) => {
+        expect($el).to.have.attr('disabled', 'disabled');
+      });
+
+    cy.get('#volume-number')
+      .type('invalid input')
+      .trigger('input');
+    cy.get('#honk-btn')
+      .then(($el) => {
+        expect($el).to.have.attr('disabled', 'disabled');
+      });
+  });
+
+  it('Error shown when input is a number outside of valid range', () => {
+    cy.get('#volume-number')
+      .clear()
+      .type('101')
+      .trigger('input')
+      .then($el => $el[0].checkValidity())
+      .should('be.false');
+    cy.get('#volume-number')
+      .clear()
+      .type('100')
+      .trigger('input')
+      .then($el => $el[0].checkValidity())
+      .should('be.true');
   });
 });
